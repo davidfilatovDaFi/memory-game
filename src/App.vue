70f803d310d694c64ee531b4ff4106177ec6b749
davidfilatovDaFi <oxelolol@gmail.com>
2023-07-11 17:43:02 +0300

    maket is ready

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
 <section class="game">
-  
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
+  <card/>
 </section>
 </template>
 
 <script>
+import card from '@/components/card.vue'
 
 export default {
   name: 'App',
   components: {
-
+    card
   }
 }
 </script>
 
 <style lang="scss">
-
+  * {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  }
+  body {
+    background-color: #ffe7d0;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .game {
+    width: 850px;
+    display: flex;
+    flex-wrap: wrap;
+  }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <img class="card_back" src="../assets/card.jpg" alt="card">
+    <img class="card_front" src="../assets/card_front.jpg" alt="card">
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  .card {
+    width: 150px;
+    height: 200px;
+    margin: 5px;
+    position: relative;
+    box-shadow: 1px 1px 1px rgba(0,0,0,.3);
+  }
+  .card_front, .card_back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+</style>
